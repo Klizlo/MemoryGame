6ef719cf7c38c705e779c17eaee0f2b5cafbd81d
Klizlo <klizlo0111@gmail.com>
2023-11-28 17:39:16 +0100

    Lab 07 part 1
    Add CirclePart and TransformIntoCard

---
+++ TransformIntoCard.swift
@@ -0,0 +1,34 @@
+//
+//  TransformIntoCard.swift
+//  MemoryGame
+//
+//  Created by student on 28/11/2023.
+//
+
+import SwiftUI
+
+struct TransformIntoCard: ViewModifier {
+    
+    let isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            base.fill(.white)
+            base.strokeBorder(lineWidth: 2)
+                .overlay(content)
+            .opacity(isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+    }
+    
+}
+
+extension View {
+    
+    func cardify(isFaceUp: Bool) -> some View {
+        modifier(TransformIntoCard(isFaceUp: isFaceUp))
+    }
+    
+}
